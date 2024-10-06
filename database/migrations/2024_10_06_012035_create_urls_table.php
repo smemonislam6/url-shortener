a4af5aa472a9ded3e425cde8fb4ea0eb018918fd
SM Emon Islam <45823216+smemonislam6@users.noreply.github.com>
2024-10-06 07:24:10 +0600

    "Added foreign key for User, long_url, short_url, and click_count columns to urls table in migration"

--- database/migrations/2024_10_06_012035_create_urls_table.php
+++ database/migrations/2024_10_06_012035_create_urls_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('urls', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('long_url');
+            $table->string('short_url');
+            $table->integer('click_count')->default(0);
             $table->timestamps();
         });
     }
